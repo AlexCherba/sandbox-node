f067a5a595bf5893a32584b17b08fa8f3aac6bf3
AlexCherba <alexander.cherba@gmail.com>
2019-05-08 14:04:25 +0300

    Added http module

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello world!');
+// https://github.com/bradtraversy/node_crash_course
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  //console.log('request:', req);
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('all okey');
+});
+
+const PORT = 5000;
+
+server.listen(5000, () => {
+  console.log(`Server running on port ${PORT}`);
+});
